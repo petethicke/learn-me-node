2529ebf034b006194fc24aed2a48b57d4b0ed5c3
Peter Thicke <peterbthicke@gmail.com>
2015-05-12 00:23:46 +0000

    finished first three exercises

--- sum.js
+++ sum.js
@@ -1,8 +1,10 @@
-function sum() {
-  for (i = 2; i < process.argv.length; i++) {
-    +process.argv[i] += [i];
-  }
+var result = 0;
+
+for (var i = 2; i < process.argv.length; i++) {
+  result += Number(process.argv[i]);
 }
 
-console.log(sum);
+console.log(result);
+
+
 
